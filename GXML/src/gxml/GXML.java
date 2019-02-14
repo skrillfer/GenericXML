488c44d2a6a59b13387620c15f4331776999ffe4
Luis Ramirez <rluis4490@gmail.com>
2019-02-14 11:52:23 -0600

    aplicacion creada

---
+++ GXML/src/gxml/GXML.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gxml;
+
+/**
+ *
+ * @author fernando
+ */
+public class GXML {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
