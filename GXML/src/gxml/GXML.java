4d1a63dca7aa6553833ed09f9234ab5405f82eac
Luis Ramirez <rluis4490@gmail.com>
2019-03-19 11:27:42 -0600

    Jar Generado

--- GXML/src/gxml/GXML.java
+++ GXML/src/gxml/GXML.java
@@ -12,12 +12,17 @@ import Analizadores.Gxml.LexGxml;
 import Analizadores.Gxml.SintacticoGxml;
 import Ast_Generator.AST_Script;
 import Estructuras.Nodo;
+import IDE.IDE_Ventana;
 import WRAPERS.PanelGenerico;
 import WRAPERS.Reproductor;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 
 /**
@@ -29,10 +34,10 @@ public class GXML {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws FileNotFoundException {
+    public  void mxxxx() {
         String num1 = "10";
         //System.out.println((num1++)*(num1--)*(num1++));
-        switch (num1) {
+        /*switch (num1) {
             case "1":
                 System.out.println("1");
                 break;
@@ -49,7 +54,7 @@ public class GXML {
                 System.out.println("defecto");
 
                 break;
-        }
+        }*/
 
         //JFrame frame = new JFrame("Capture");
         //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -94,15 +99,49 @@ public class GXML {
         //System.out.println(sss.esTipo(".3."));
     }
 
-    public int ackermann(int m, int n) {
-        //System.out.println("entro");
-        if (m == 0) {
-            return (n + 1);
-        } else if (m > 0 && n == 0) {
-            return ackermann(m - 1, 1);
-        } else {
-            return ackermann(m - 1, ackermann(m, n - 1));
+    public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(IDE_Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(IDE_Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(IDE_Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(IDE_Ventana.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+
+        try (BufferedReader br = new BufferedReader(new FileReader("dir_root.txt"))) {
+            String sCurrentLine;
+            if ((sCurrentLine = br.readLine()) != null) {
+
+                File fi = new File(sCurrentLine);
+                if (fi.exists() && fi.isDirectory()) {
+                    IDE_Ventana.JTREE_DIR_ROOT = fi.getAbsolutePath();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    new IDE_Ventana().setVisible(true);
+                } catch (IOException ex) {
+                    Logger.getLogger(IDE_Ventana.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
     }
+
 
 }
